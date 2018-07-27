6e8b1a8080cab0285cf9d41f9cb9ab359605f211
kate harris <kateharris03@gmail.com>
2018-07-27 16:38:17 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,6 +1,16 @@
 class GardenGnome
   attr_accessor :name, :age, :gluten_allergy
-  def initialize(personality)
+  def initialize(personality, hat_color = "red")
     @personality = "evil"
+    @hat_color = hat_color
   end
-  def personality
+  def personality = (personality)
+    @personality = "eil"
+  end
+  def gnaw
+    return "Gnawing on a tree!!!"
+  end
+  def shout
+    return "GNARLY!!!"
+  end
+  puts "Hello, humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
