9977bcdfcf50070f1bd87c49284d357e1dc8c8b5
kate harris <kateharris03@gmail.com>
2018-07-27 16:44:04 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,7 +1,6 @@
 class GardenGnome
   attr_accessor :name, :age, :gluten_allergy
   def initialize(hat_color = "red")
-    @personality = "evil"
     @hat_color = hat_color
   end
   def personality= (personality)
